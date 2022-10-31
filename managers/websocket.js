d59179d08d500c27b6e9894185b72316553fc673
Santiago Posse <smposse@gmail.com>
2022-10-31 20:18:03 -0300

    Empezando desafío Sql

---
+++ managers/websocket.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+class ContenedorWebsocket {
+  constructor(nameFile) {
+    this.nameFile = path.join(__dirname, `../files/${nameFile}`);
+  }
+  save = async (obj) => {
+    await fs.promises.writeFile(this.nameFile, JSON.stringify(obj, null, 2));
+  };
+  getAll = async () => {
+    if (this.nameFile) {
+      const contenido = await fs.promises.readFile(this.nameFile, "utf-8");
+      if (contenido) {
+        const mensajes = JSON.parse(contenido);
+        return mensajes;
+      } else {
+      }
+    }
+  };
+}
+module.exports = ContenedorWebsocket;
